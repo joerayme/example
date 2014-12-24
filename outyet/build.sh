0204c5802ac46bf3aa482136fad1292b6a0c49e9
Joe Ray <jray@thoughtworks.com>
2014-12-24 10:01:26 +0000

    Update build script

--- outyet/build.sh
+++ outyet/build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-export DOCKER_HOST="tcp://`/sbin/ip route|awk '/default/ { print $3 }'`:2376"
-export DOCKER_TLS_VERIFY=1
-export DOCKER_CERT_PATH=/var/go/certs
-
-docker build --rm --tag outyet:v$GO_PIPELINE_LABEL .
-docker run --volume build:/go/bin --rm outyet:v$GO_PIPELINE_LABEL ls /go/bin/outyet
+run_in_container --name outyet --tag v$GO_PIPELINE_LABEL --command "ls /go/bin/outyet"
